8c38f5d058bc76936bcd1dc3ccc96ecee21955db
yangtingting <haoyue3021@163.com>
2020-12-31 09:44:21 +0800

    Saving before ejecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from "antd-mobile";
 import './App.css';
+import Project from './containers/Project/index'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Project />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type="warning">我是一个按钮</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/containers/Project/index.tsx
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+export default class index extends PureComponent {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>我是一个组件</div>
+        )
+    }
+}
